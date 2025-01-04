1ebe3deaa6fdd3a202788b358bb11f07186cbe69
Estela Martinez <emargar1@epsa.upv.es>
2025-01-04 14:41:10 +0100

    Definición Drawing añadida

--- Drawing.h
+++ Drawing.h
@@ -16,8 +16,8 @@ class Drawing {
 		
 		void add_front(Shape* shape);
 		void add_back(Shape* shape);
-		void print_all();
-		double get_area_all_circles();
+		void print_all() const;
+		double get_area_all_circles() const;
 		void move_squares(double incX, double incY);
 };
 
